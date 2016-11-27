80ca1aee93ae87bc9afa30e72469fcef45b7aec6
simon <simon@nasrallah@bbc.com>
2016-11-27 19:21:09 +0000

    added line and char count plus a test

--- src/wc.py
+++ src/wc.py
@@ -12,7 +12,15 @@ def open_file(file_name):
     with open(file_name, 'r') as f:
        return f.read()
 
-def wc(text):
+def line_count(file_name):
+    with open(file_name, 'r') as f:
+        return len(f.readlines())
+
+def char_count(text):
+    logging.debug('original text: [{}]'.format(text))
+    return len(text)
+
+def word_count(text):
     pat = re.compile(WORD) 
     logging.debug('original text: [{}]'.format(text))
     clean_text = text.translate(None, string.punctuation)
@@ -22,14 +30,23 @@ def wc(text):
 
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
-    level = logging.INFO
+    level = logging.DEBUG
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=level)
     logging.debug('Started DEBUG')
+    logging.debug('args {}'.format(sys.argv))
     
     arg1 = ''
-    if len(sys.argv) > 1: 
+    if len(sys.argv) == 2: 
         arg1 = sys.argv[1]
-        logging.debug('opening file {}...'.format(arg1,))
-    	print wc(open_file(arg1))
+        logging.debug('opening file for word count {}...'.format(arg1,))
+    	print word_count(open_file(arg1))
+    elif len(sys.argv) == 3 and sys.argv[1] == '-m': 
+        arg2 = sys.argv[2]
+        logging.debug('opening file for char count {}...'.format(arg2,))
+        print char_count(open_file(arg2))
+    elif len(sys.argv) == 3 and sys.argv[1] == '-l': 
+        arg2 = sys.argv[2]
+        logging.debug('opening file for char count {}...'.format(arg2,))
+        print line_count(arg2)
     else:
-        print 'no file was specified' 
+        print 'no file was specified or args' 
--- tests/test_wc.py
+++ tests/test_wc.py
@@ -1,17 +1,22 @@
 import unittest
 import sys
 sys.path.append('../src')
-from wc import wc
+from wc import *
 
 class TestWC(unittest.TestCase):
     def test_words(self):
         text = """a bb2\t ccc_3
 	dddd@@@    eeeee***"""
-        self.assertEquals(5, wc(text))
+        self.assertEquals(5, word_count(text))
 
     def test_word_with_quote_should_be_one_word(self):
         text = """word's"""
-        self.assertEquals(1, wc(text))
+        self.assertEquals(1, word_count(text))
+    
+    def test_count_word_should_be_33(self):
+        text = """a bb2\t ccc_3
+	dddd@@@    eeeee***"""
+        self.assertEquals(33, char_count(text))
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
